da467940b7ac50af50ddfe7db382a083bcc4ee65
COISPA Tecnologia & Ricerca <60480840+COISPA@users.noreply.github.com>
2021-11-22 11:25:26 +0100

    Update check_LFD.r

--- R/check_LFD.r
+++ R/check_LFD.r
@@ -2,18 +2,18 @@
 #'
 #' @param data
 #'
-#' @return comparison plot of LFDs among the  years and length range of the data
+#' @return comparison plot of LFDs among the  years and check of length range of the data using allowed range
 #' @export
 #'
 #' @examples check_LFD(dps9)
 #'
-check_LFD <- function(data,min_len=1,max_len=1000) {print(ggplot(data=data, aes(x=Length.class,y= Number.at.length)) + geom_histogram(stat="identity",colour = "blue", fill = "blue", binwidth = 0.5) + facet_grid(Year~ .))
+check_LFD <- function(data_example,min_len=1,max_len=1000) {print(ggplot(data_example=data_example, aes(x=Length.class,y= Number.at.length)) + geom_histogram(stat="identity",colour = "blue", fill = "blue", binwidth = 0.5) + facet_grid(Year~ .))
 
-paste("The maximum length class in the data is: ",max(data$Length.class)," mm",sep="")
-paste("The minimum length class in the data is: ",min(data$Length.class)," mm",sep="")
+paste("The maximum length class in the data is: ",max(data_example$Length.class)," mm",sep="")
+paste("The minimum length class in the data is: ",min(data_example$Length.class)," mm",sep="")
 
-error_min_length=data[data$Length.class<min_len, ]
-error_max_length=data[data$Length.class>max_len, ]
+error_min_length=data_example[data_example$Length.class<min_len, ]
+error_max_length=data_example[data_example$Length.class>max_len, ]
 
 if (nrow(error_min_length)!=0 | nrow(error_max_length)!=0){
 
