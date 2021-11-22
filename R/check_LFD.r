fee871d55972089c5699659bf4da25835f33edd6
COISPA Tecnologia & Ricerca <60480840+COISPA@users.noreply.github.com>
2021-11-22 11:14:18 +0100

    update check lfd

--- R/check_LFD.r
+++ R/check_LFD.r
@@ -7,9 +7,26 @@
 #'
 #' @examples check_LFD(dps9)
 #'
-check_LFD <- function(data) {print(ggplot(data=data, aes(x=Length.class,y= Number.at.length)) + geom_histogram(stat="identity",colour = "blue", fill = "blue", binwidth = 0.5) + facet_grid(Year~ .))
+check_LFD <- function(data,min_len=1,max_len=1000) {print(ggplot(data=data, aes(x=Length.class,y= Number.at.length)) + geom_histogram(stat="identity",colour = "blue", fill = "blue", binwidth = 0.5) + facet_grid(Year~ .))
 
 paste("The maximum length class in the data is: ",max(data$Length.class)," mm",sep="")
 paste("The minimum length class in the data is: ",min(data$Length.class)," mm",sep="")
+
+error_min_length=data[data$Length.class<min_len, ]
+error_max_length=data[data$Length.class>max_len, ]
+
+if (nrow(error_min_length)!=0 | nrow(error_max_length)!=0){
+
+    print(paste("Records with length outside the allowed range:"), quote=F)
+    if(nrow(error_min_length)!=0){
+        print(unique(as.character(error_min_length$Trip.code))  )
+    }
+    if(nrow(error_max_length)!=0){
+        print(unique(as.character(error_max_length$Trip.code) ))
+    }
+} else {
+    print("No error occurred", quote=F)
+
+}
 }
 
