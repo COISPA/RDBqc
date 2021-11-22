58821f26df99e861b54e9f4ef38b0b534a2b58d4
COISPA Tecnologia & Ricerca <60480840+COISPA@users.noreply.github.com>
2021-11-22 11:31:20 +0100

    check_LFD_comm_cat

---
+++ R/check_LFD_comm_cat.R
@@ -0,0 +1,19 @@
+
+#' Title
+#'
+#' @param data_example
+#'
+#' @return plot
+#' @export
+#'
+#' @examples check_LFD_comm_cat(data_example)
+check_LFD_comm_cat <- function(data_example){
+    ggplot(data=data_example, aes(x=Length.class,y= Number.at.length)) + geom_histogram(stat="identity",colour = "tan2", fill = "tan2", binwidth = 0.5) + facet_grid(Year~ Commercial.size.category)
+
+    pivot_min=aggregate(data_example$Length.class,by=list(data_example$Year,data_example$Commercial.size.category),FUN="min")
+    colnames(pivot_min)=c("Year","Commercial_size_category","Min")
+    pivot_max=aggregate(data_example$Length.class,by=list(data_example$Year,data_example$Commercial.size.category),FUN="max")
+    colnames(pivot_max)=c("Year","Commercial_size_category","Max")
+    merge(pivot_min,pivot_max, by=c("Year","Commercial_size_category"))
+
+    }
