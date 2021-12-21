c7a82f887643b244d8a2cee223166ec717459a8c
COISPA Tecnologia & Ricerca <60480840+COISPA@users.noreply.github.com>
2021-12-21 15:01:25 +0100

    update check_CL

--- R/check_CL.R
+++ R/check_CL.R
@@ -1,12 +1,13 @@
 
-#' Title
+#' Quality checks on CL RCG table
 #'
-#' @param data_exampleCL
+#' @param data_exampleCL Landing table in RCG CL format
 #'
-#' @return
+#' @return Checks_CL list of tables for temporal, spatial, species and metier coverage
 #' @export
 #'
-#' @examples
+#' @examples check_CL(data_exampleCL)
+#'
 check_CL <- function(data_exampleCL) {
 
 temp_covL=aggregate(data_exampleCL$landWt,by=list(data_exampleCL$year,data_exampleCL$quarter,data_exampleCL$month),FUN="sum")
@@ -19,6 +20,27 @@ if(length(which(is.na(data_exampleCL$vslFlgCtry)))>0) data_exampleCL[which(is.na
 if(length(which(is.na(data_exampleCL$rect)))>0) data_exampleCL[which(is.na(data_exampleCL$rect)),]$rect<-999
 if(length(which(is.na(data_exampleCL$subRect)))>0) data_exampleCL[which(is.na(data_exampleCL$subRect)),]$subRect<-999
 
+spat_covLV=aggregate(data_exampleCL$landValue,by=list(data_exampleCL$landCtry,data_exampleCL$vslFlgCtry,data_exampleCL$area,data_exampleCL$rect,data_exampleCL$subRect, data_exampleCL$harbour),FUN="sum")
+colnames(spat_covLV)=c("LandCtry","VslFlgCtry","Area","Rect", "SubRect", "Harbour", "Sum_LandingsValue")
+
 spat_covL=aggregate(data_exampleCL$landWt,by=list(data_exampleCL$landCtry,data_exampleCL$vslFlgCtry,data_exampleCL$area,data_exampleCL$rect,data_exampleCL$subRect, data_exampleCL$harbour),FUN="sum")
-colnames(temp_covL)=c("Year","Quarter","Month","Sum_Landings")
+colnames(spat_covL)=c("LandCtry","VslFlgCtry","Area","Rect", "SubRect", "Harbour", "Sum_Landings")
+
+if(length(which(is.na(data_exampleCL$foCatEu5)))>0) data_exampleCL[which(is.na(data_exampleCL$foCatEu5)),]$foCatEu5<-999
+if(length(which(is.na(data_exampleCL$foCatEu6)))>0) data_exampleCL[which(is.na(data_exampleCL$foCatEu6)),]$foCatEu6<-999
+
+spe_cov_L=aggregate(data_exampleCL$landWt,by=list(data_exampleCL$year,data_exampleCL$taxon, data_exampleCL$foCatEu5, data_exampleCL$foCatEu6),FUN="sum")
+colnames(spe_cov_L)=c("Year","Species","foCatEu5","foCatEu6", "Sum_Landings")
+
+spe_cov_LV=aggregate(data_exampleCL$landValue,by=list(data_exampleCL$year,data_exampleCL$taxon, data_exampleCL$foCatEu5, data_exampleCL$foCatEu6),FUN="sum")
+colnames(spe_cov_LV)=c("Year","Species","foCatEu5","foCatEu6", "Sum_LandingsValue")
+
+Checks_CL=list(temp_covL,temp_covLV,spat_covL,spat_covLV,spe_cov_L,spe_cov_LV)
+
+library(ggplot2)
+ggplot(data=spe_cov_L, aes(x=Year,y= Sum_Landings,colour=Species)) + geom_line(stat="identity") + geom_point(stat="identity") + facet_grid(Year~foCatEu6)
+
+
+return(Checks_CL)
+
 }
