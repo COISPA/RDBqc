e084037d997bb6156bff95e96bf94a266d2fa260
COISPA Tecnologia & Ricerca <60480840+COISPA@users.noreply.github.com>
2021-12-27 14:35:01 +0100

    check loc

---
+++ R/circabc.R
@@ -0,0 +1,6 @@
+#' Ports coordinate according to codification CIRCABC
+#' @name circabc
+#' @docType data
+#' @author CIRCABC \email{https://circabc.europa.eu/faces/jsp/extension/wai/navigation/container.jsp}
+#' @keywords CIRCABC Harbour
+"circabc"
