17709ba8b377fca733e7a8124a7636bed0f73e4e
COISPA Tecnologia & Ricerca <60480840+COISPA@users.noreply.github.com>
2021-12-27 14:43:16 +0100

    update circabc

--- R/circabc.R
+++ R/circabc.R
@@ -1,6 +1,6 @@
-#' Ports coordinate according to codification CIRCABC
+#' Ports coordinates according to codification CIRCABC
 #' @name circabc
 #' @docType data
-#' @author CIRCABC \email{https://circabc.europa.eu/faces/jsp/extension/wai/navigation/container.jsp}
+#' @author CIRCABC \email{https://circabc.europa.eu}
 #' @keywords CIRCABC Harbour
 "circabc"
