839c80532af13baaee60881d2af2640ca01dbb27
Walter <info@coispa.eu>
2022-04-04 15:11:30 +0200

    wz_20220404_4

--- RDBqc/R/MEDBS_Discard_coverage.R
+++ RDBqc/R/MEDBS_Discard_coverage.R
@@ -6,10 +6,10 @@
 #' @param GSA GSA (Geographical sub-area (GFCM sensu))
 #' @return summary table and plots
 #' @export
-#' @examples Discard_coverage(Discard_tab_example,"DPS","ITA","9")
+#' @examples MEDBS_discard_coverage(Discard_tab_example,"DPS","ITA","9")
 #' @import ggplot2 dplyr
 #' @importFrom utils globalVariables
-Discard_coverage<-function(Discard_tab,SP,MS,GSA){
+MEDBS_discard_coverage<-function(Discard_tab,SP,MS,GSA){
 
     Discard_tab=Discard_tab[Discard_tab$SPECIES==SP & Discard_tab$COUNTRY==MS & Discard_tab$AREA==GSA,]
 
@@ -17,7 +17,7 @@ Discard_coverage<-function(Discard_tab,SP,MS,GSA){
 
 
     Summary_land_wt=aggregate(Discard_tab[,2:12]$DISCARDS,by=list(Discard_tab$COUNTRY, Discard_tab$YEAR, Discard_tab$QUARTER, Discard_tab$VESSEL_LENGTH, Discard_tab$GEAR, Discard_tab$MESH_SIZE_RANGE, Discard_tab$FISHERY,  Discard_tab$AREA,Discard_tab$SPECIES),FUN="sum")
-    colnames(Summary_land_wt)=c("COUNTRY", "YEAR", "QUARTER", "VESSEL_LENGTH", "GEAR", "MESH_SIZE_RANGE", "FISHERY",  "AREA","SPECIES",  "LANDINGS" )
+    colnames(Summary_land_wt)=c("COUNTRY", "YEAR", "QUARTER", "VESSEL_LENGTH", "GEAR", "MESH_SIZE_RANGE", "FISHERY",  "AREA","SPECIES",  "DISCARDS" )
 
     Summary_land_wt[1:nrow(Summary_land_wt),1:ncol(Summary_land_wt)]
 
@@ -38,11 +38,11 @@ Discard_coverage<-function(Discard_tab,SP,MS,GSA){
     # data <-  data[data$Landing_tab>0,]
 
 
-    ggplot(data, aes(x=YEAR, y=DISCARDS, fill=GEAR)) +
+    p <- ggplot(data, aes(x=YEAR, y=DISCARDS, fill=GEAR)) +
         geom_area(size=0.5, colour="black")+theme_bw()+
         ggtitle(paste0("Discards of ",SP, " in ", MS,"_GSA",GSA))+xlab("")+ylab("tonnes")+theme(legend.position = "bottom")+
         scale_x_continuous(breaks=seq(min(data$YEAR),max(data$YEAR),2))
-
+    print(p)
 
 
     return(Summary_land_wt)
